d6b73b13f191caf4850b92399776c9fde35b8b63
Lars <l.cornelissen5@student.han.nl>
2023-01-23 13:17:54 +0100

    src

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import torch
 from diffusers import StableDiffusionPipeline
 
 model_id = "CompVis/stable-diffusion-v1-4"
-device = "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
 pipe = pipe.to(device)
 
 prompt = "a photo of an astronaut riding a horse on mars"
-image = pipe(prompt).images[0]  
-    
-image.save("astronaut_rides_horse.png")
+
+images = pipe(prompt, width=256, height=256).images
+
+for i in range(len(images)):
+    images[i].save(f"astronaut_rides_horse{i}.png")
